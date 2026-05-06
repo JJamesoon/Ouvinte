b3f12ddada0cde001ae49d9ad9a4781e0c7c438c
Jameson <141678211+JJamesoon@users.noreply.github.com>
2026-05-06 00:08:46 -0300

    Update index.js

--- index.js
+++ index.js
@@ -101,4 +101,4 @@ app.post('/webhook-whatsapp', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Serviço ativo na porta ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Serviço ativo na porta ${PORT}`));
